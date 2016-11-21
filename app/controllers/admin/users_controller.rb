e202a03add6f1a464d0c8478d5c3e654a96b0e3c
quangtien133 <phanqtien@gmail.com>
2016-11-21 13:16:03 +0700

    admin manage users

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,31 @@
+class Admin::UsersController < ApplicationController
+  layout "layouts/admin_layout"
+  load_and_authorize_resource
+  before_action :load_user, only: :destroy
+
+  def index
+    @users = User.search(params[:key]).page params[:page]
+  end
+
+  def destroy
+    unless @user.destroy
+      flash[:warning] = t :failed, name: @user.name
+      redirect_to users
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name,
+      :birthday, :email, :sex, :address,
+      :password, :password_confirmation, :avatar
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    unless @user
+      flash[:danger] = t :user_not_found
+      redirect_to root_url
+    end
+  end
+end
